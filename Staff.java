cac3ec501f555840a503920093509bef44733a5d
JoeyJiayi <84704315+JoeyJiayi@users.noreply.github.com>
2021-11-06 23:19:36 +0800

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -1,54 +1,59 @@
-import java.util.Scanner;
+import java.io.Serializable;
 
-public class Staff {
-
-	private Table table;
-	private String staffName;
-	private String gender;
-	private long identificationNumber;
-	private String title;
-	private int maximumTables;
-	private int currentTables;
-	static private int totalNumber = 0;
-
-	public Staff(){
-
-		table = null;
-		staffName = null;
-		gender = null;
-		identificationNumber = 0;
-		title = null;
-		totalNumber++;
-		maximumTables = 3;
-		currentTables = 0;
-	}
-
-	public Staff(String staffName, String gender, long identificationNumber, String title, int maximumTables){
-		this.staffName = staffName;
-		this.gender = gender;
-		this.identificationNumber = identificationNumber;
-		this.title = title;
-		totalNumber++;
-		this.maximumTables = maximumTables;
-		this.currentTables = 0;
-	}
-
-
-	public boolean serveMoreTables(){
-		if((maximumTables-currentTables) > 0 ) return true;
-		else return false;
-	}
-
-	public void allocateTable(Table table){
-		this.table = table;
-		if(currentTables<maximumTables) this.currentTables++;
-	}
-
-	public void deallocateTable(){
-		this.table = null;
-		if(this.currentTables>0) this.currentTables--;
-	}
-
-
-
+public class Staff implements Serializable {
+   private String staffName;
+   private String gender;
+   private long id;
+   private String title;
+   
+   private Table table;
+   private static int maximumTables = 3;
+   private int currentTables;
+  
+   
+   // Constructor for Staff
+   public Staff(String staffName, String gender, long id, String title){
+      this.staffName = staffName;
+      this.gender = gender;
+      this.id = id;
+      this.title = title;
+      this.maximumTables = maximumTables;
+      this.currentTables = 0;
+   }
+   
+   
+   public String getStaffName(){
+      return this.staffName;
+   }
+   
+   public String getStaffGender(){
+      return this.gender;
+   }
+   
+   public long getStaffID(){
+      return this.id;
+   }
+   
+   public String getStaffTitle(){
+      return this.title;
+   }
+   
+   public boolean serveMoreTables(){
+      if((maximumTables-currentTables) > 0 ) 
+         return true;
+      else 
+         return false;
+   }
+   
+   public void allocateTable(Table table){
+      this.table = table;
+      if(serveMoreTables() == true) 
+         this.currentTables++;
+   }
+   
+   public void deallocateTable(){
+      this.table = null;
+      if(this.currentTables>0) 
+         this.currentTables--;
+   }
 }
