078b314bf0e967f15c3ee35a9e70885edbd9ee8e
JoeyJiayi <84704315+JoeyJiayi@users.noreply.github.com>
2021-11-06 15:39:23 +0800

    Update PromotionalPackage.java

--- PromotionalPackage.java
+++ PromotionalPackage.java
@@ -3,15 +3,14 @@ import java.util.ArrayList;
 import java.io.Serializable;
 
 public class PromotionalPackage implements Serializable{
-
-   
    private static Scanner sc = new Scanner(System.in);
-
+   
    public String packageName;
    public String packageDescription;
    public double packagePrice;
    public ArrayList<Item> packageItems;
-      
+   
+   //constructor for PromotionalPackage
    public PromotionalPackage(String packageName, String packageDescription, double packagePrice, ArrayList<Item> packageItems){
       this.packageName = packageName;
       this.packageDescription = packageDescription;
@@ -19,26 +18,32 @@ public class PromotionalPackage implements Serializable{
       this.packageItems = packageItems;
    }
    
+   //accessors
    public String getPackageName(){
       return this.packageName;
+   }
+   
+   public void setPackageName(String packageName){
+      this.packageName = packageName;
    }
    
    public String getPackageDescription(){
       return this.packageDescription;
    }
    
+   public void setPackageDescription(String packageDescription){
+      this.packageDescription = packageDescription;
+   }
+   
    public double getPackagePrice(){
       return this.packagePrice;
+   }
+   
+   public void setPackagePrice(double packagePrice){
+      this.packagePrice = packagePrice;
    }
    
    public ArrayList<Item> getPackageItems(){
       return this.packageItems;
    }
-   
-
-
-  
-
-
-
 }
