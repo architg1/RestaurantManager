99eaf456562afec2e710bc730742797f7c36dba8
JoeyJiayi <84704315+JoeyJiayi@users.noreply.github.com>
2021-11-12 17:58:42 +0800

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -1,59 +1,110 @@
+import java.time.LocalTime;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.io.Serializable;
 
-public class Staff implements Serializable {
-   private String staffName;
-   private String gender;
-   private long id;
-   private String title;
+public class Reservation implements Serializable, Runnable
+{
+	
+	//Table table;
+	/*
+	protected Calendar orderDateTime; //Check to see how we want to standardize the date time formats
+	protected int seatsBooked;
+	protected String reservationName;
+	protected String tableID;
+	protected long reservationContact;
+	protected boolean isSuccess;
+	protected boolean isMember;
+	protected int reservationID;
+	*/
+	
+   private boolean isReserved;
+   private String reservationName;
+   private long reservationContact;
+   private boolean reservationMembership;
+   private LocalTime reservationTime;
+   private LocalDate reservationDate;
+   private int reservedSeating;
+
+   private Table reserveTable;
    
-   private Table table;
-   private int maximumTables = 3;
-   private int currentTables;
-  
-   
-   // Constructor for Staff
-   public Staff(String staffName, String gender, long id, String title, int maximumTables){
-      this.staffName = staffName;
-      this.gender = gender;
-      this.id = id;
-      this.title = title;
-      this.maximumTables = maximumTables;
-      this.currentTables = 0;
+   ReservationCtrl reservationctrl = new ReservationCtrl();
+	
+	
+	//How many tables do we want? Fixed number or ArrayList flexible (table or reservation class?)
+	//protected Reservation[] rList = new Reservation[20];
+
+	//private ArrayList<Reservation> reservationList = new ArrayList<Reservation>();
+
+	//datetime, pax,name,contact,etc
+
+   public Reservation (String reservationName,
+   					long reservationContact, 
+   					boolean reservationMembership, 
+   					LocalDate reservationDate, 
+   					LocalTime reservationTime, int reservedSeating){
+      this.reservationName = reservationName;
+      this.reservationContact = reservationContact;
+      this.reservationMembership = reservationMembership;
+      this.reservationTime = reservationTime;
+      this.reservationDate = reservationDate;
+      this.reservedSeating = reservedSeating;
    }
-   
-   
-   public String getStaffName(){
-      return this.staffName;
+	
+	/**
+	 * get the name of the customer who made this reservation
+	 * @return this reservation's name
+	 */
+   public String getCustomerName()
+   { 
+      return this.reservationName; 
    }
-   
-   public String getStaffGender(){
-      return this.gender;
+	
+	/**
+	 * get the contact number of the customer who made this reservation
+	 * @return this reservation's contact number
+	 */
+   public long getCustomerContact()
+   { 
+      return this.reservationContact; 
    }
-   
-   public long getStaffID(){
-      return this.id;
+	
+	/**
+	 * get the number of expected people for this reservation
+	 * @return this reservation's number of expected people
+	 */
+   public int getNumPax()
+   { 
+      return this.reservedSeating; 
    }
-   
-   public String getStaffTitle(){
-      return this.title;
+	
+	
+	/**
+	 * get the expected arrival time of this reservation
+	 * @return this reservation's expected arrival time
+	 */
+   public LocalTime getArrivalTime()
+   {
+      return this.reservationTime; 
    }
-   
-   public boolean serveMoreTables(){
-      if((maximumTables-currentTables) > 0 ) 
-         return true;
-      else 
-         return false;
+	
+   public LocalDate getReservationDate()
+   {
+      return this.reservationDate;
    }
-   
-   public void allocateTable(Table table){
-      this.table = table;
-      if(serveMoreTables() == true) 
-         this.currentTables++;
+
+   public void run()
+   {
+      while(true){
+         int time = LocalTime.now().toSecondOfDay() - reservationTime.toSecondOfDay();
+         if(time>=30)
+         {
+            reservationctrl.cancelReservation(this.reservationName, this.reservationContact);
+            break;
+         }
+      }
    }
-   
-   public void deallocateTable(){
-      this.table = null;
-      if(this.currentTables>0) 
-         this.currentTables--;
-   }
+
 }
