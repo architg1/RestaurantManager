d6bae0a633a955fc7fb0ffed8d0be818d3564f57
JoeyJiayi <84704315+JoeyJiayi@users.noreply.github.com>
2021-11-14 17:57:26 +0800

    Create Invoice.java

---
+++ Invoice.java
@@ -0,0 +1,13 @@
+import java.time.LocalTime;
+import java.util.*;
+import java.io.Serializable;
+
+public class Invoice implements Serializable {
+   private Order order
+   
+   //Constructor for order
+   public Invoice(order){
+     
+   }
+
+}
