587e7aa79283b949bbc086e28a2cfbac08d3bd94
JoeyJiayi <84704315+JoeyJiayi@users.noreply.github.com>
2021-11-14 23:30:06 +0800

    Update BaseCtrl.java

--- BaseCtrl.java
+++ BaseCtrl.java
@@ -4,23 +4,23 @@ import java.util.*;
 
 public abstract class BaseCtrl {
 	
-	   @SuppressWarnings("SameParameterValue")
-	   public static int doCtrlChoice(int max) {
-	        Scanner choiceInput = new Scanner(System.in);
-	        int selection;
-	        do {
-	            System.out.print("Enter choice: ");
-	            try {
-	                selection = choiceInput.nextInt();
-	                if ((selection > max) || selection < 1)
-	                    System.out.println("Invalid Selection\n. Please select an option from 1 - " + max);
-	            } catch (InputMismatchException e) {
-	                selection = -1;
-	                System.out.println("Invalid Input\n. Please enter a valid number");
-	                choiceInput.nextLine();
-	            }
-	            System.out.println();
-	        } while ((selection > max) || selection < 1);
-	        return selection;
-	    }
+   @SuppressWarnings("SameParameterValue")
+      public static int doCtrlChoice(int max) {
+      Scanner choiceInput = new Scanner(System.in);
+      int selection;
+      do {
+         System.out.print("Enter choice: ");
+         try {
+            selection = choiceInput.nextInt();
+            if ((selection > max) || selection < 1)
+               System.out.println("Invalid Selection.\n Please select an option from 1 - " + max);
+         } catch (InputMismatchException e) {
+            selection = -1;
+            System.out.println("Invalid Input\n. Please enter a valid number");
+            choiceInput.nextLine();
+         }
+         System.out.println();
+      } while ((selection > max) || selection < 1);
+      return selection;
+   }
 }
